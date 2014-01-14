2ba60e0dd4d8fc43676e7ef9da750322fe854285
Volodia MANDAUD <llvolo2@gmail.com>
2014-01-14 19:24:36 +0100

    Rendu de la page web RenduHTML.xaml & RenduHTML.xaml.cs fonctionnel

--- ChtemeleSurfaceApplication/ChtemeleSurfaceApplication/RenduHTML.xaml.cs
+++ ChtemeleSurfaceApplication/ChtemeleSurfaceApplication/RenduHTML.xaml.cs
@@ -21,9 +21,37 @@ namespace ChtemeleSurfaceApplication
     /// </summary>
     public partial class RenduHTML : ScatterViewItem
     {
+        // Variable contenant l'URL ou URI du fichier
+        private string fileURL;
+
+        // Constructeur
         public RenduHTML()
         {
             InitializeComponent();
+            
+            // Envoie l'URL par méthode SET -> utilisé lors du chargement d'un nouveau fichier
+            // Le site doit ABSOLULENT avoir un affichage adaptatif selon la résolution !
+            setFileUrl("http://www.lukew.com/ff/entry.asp?933");
+            // Envoie la source (page web) a afficher
+            WebRenderer();
+        }
+
+        
+        // GET & SET
+        public string getFileUrl()
+        {
+            return fileURL;
+        }
+        public void setFileUrl(string fileUrl)
+        {
+            fileURL = fileUrl;
+        }
+
+        // Initialise la source de la page à afficher
+        private void WebRenderer()
+        {
+            WB1.Navigate(new Uri(getFileUrl()));
+
         }
     }
 }
