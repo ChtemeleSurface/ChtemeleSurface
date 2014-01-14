f8c3ef75387d46d58d9e5dc375c486fb965137c8
Volodia MANDAUD <llvolo2@gmail.com>
2014-01-14 13:35:12 +0100

    Ajout des Class du diagramme Game Faudrait qu'on discute du diagramme

---
+++ ChtemeleSurfaceApplication/ChtemeleSurfaceApplication/Class_CardEvent.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ChtemeleSurfaceApplication
+{
+    class Class_CardEvent : Class_Card
+    {
+
+    }
+}
---
+++ ChtemeleSurfaceApplication/ChtemeleSurfaceApplication/Class_Combo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ChtemeleSurfaceApplication
+{
+    class Class_Combo
+    {
+        // Numéro du code
+        private int code;
+        // ??? Score
+        private int score;
+        // Tableau du dernier combo, 12 étant le nb max de carte en main
+        private fixed int lastCombo [12];
+    }
+}
