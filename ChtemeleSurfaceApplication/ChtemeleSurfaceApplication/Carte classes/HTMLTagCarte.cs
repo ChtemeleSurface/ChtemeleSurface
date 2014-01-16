d5872b467ddd167861f03578a747daab4756d343
sexysoushi <barbara1603@hotmail.com>
2014-01-16 11:54:03 +0100

    enum des balises dans HTMLTagCarte

--- ChtemeleSurfaceApplication/ChtemeleSurfaceApplication/Carte classes/HTMLTagCarte.cs
+++ ChtemeleSurfaceApplication/ChtemeleSurfaceApplication/Carte classes/HTMLTagCarte.cs
@@ -12,6 +12,44 @@ namespace ChtemeleSurfaceApplication.Carte_classes
         private HtmlTag _tag;
         private HtmlTag.HTMLTagType _type;
 
+        public enum AvailableOpenTags
+        {
+            H1,
+            H2,
+            P,
+            DIV,
+            BLOCKQUOTE,
+            HEADER,
+            FOOTER,
+            ASIDE,
+            STRONG,
+            EM,
+            A
+        }
+
+        public enum AvailableEndTags
+        {
+            H1,
+            H2,
+            P,
+            DIV,
+            BLOCKQUOTE,
+            HEADER,
+            FOOTER,
+            ASIDE,
+            STRONG,
+            EM,
+            A
+        };
+
+        public enum AvailableSingleTags
+        {
+            BR,
+            HR,
+            IMG
+
+        };
+
         public HTMLTagCarte()
         {
             _type = HtmlTag.HTMLTagType.OPENTAG; 
