4ef3be5fe23317d072abfffa2ff3e0727d65927c
Janisse <j_clement_42@live.fr>
2014-01-16 15:30:09 +0100

    - Recorrige le probleme des fonctions mal définit  - supprime le menu inutile a la doc

--- ChtemeleSurfaceApplication/ChtemeleSurfaceApplication/HTML classes/HtmlPage.cs
+++ ChtemeleSurfaceApplication/ChtemeleSurfaceApplication/HTML classes/HtmlPage.cs
@@ -86,7 +86,7 @@ namespace ChtemeleSurfaceApplication.HTML_classes
         public string renderHTML(){
             string res = "";
 
-            res += _mainTag.RenderHTML();
+            res += _mainTag.renderHTML();
 
             return res;
         }
