121c05e9f292b3873cf093dce7faae668bc64b32
Janisse <j_clement_42@live.fr>
2014-01-17 12:28:05 +0100

    - Bon, après avoir chercher tout le matin, on a enfin nos pages webs relatives d'afficher =)

--- ChtemeleSurfaceApplication/ChtemeleSurfaceApplication/RenduHTML.xaml.cs
+++ ChtemeleSurfaceApplication/ChtemeleSurfaceApplication/RenduHTML.xaml.cs
@@ -28,32 +28,9 @@ namespace ChtemeleSurfaceApplication
         public RenduHTML()
         {
             InitializeComponent();
-            
-            // Envoie l'URL par méthode SET -> utilisé lors du chargement d'un nouveau fichier
-            // Le site doit ABSOLULENT avoir un affichage adaptatif selon la résolution !
+            fileURL = System.IO.Path.GetFullPath("./Resources/Documentation/index.html");
 
-            //Uri uri = new Uri(@"index.html", UriKind.Relative);
-           //Uri uri = new Uri("Ressources/Documentation/index.html", UriKind.Relative);
-            //string temp = uri.ToString(); //uri.OriginalString
-           // string temp2 = uri.OriginalString;
-
-
-            //setFileUrl(temp);  // http://www.lukew.com/ff/entry.asp?933  // F:/DUT INFORMATIQUE/2ND/TUT/ChtemeleSurface/ChtemeleSurfaceApplication/Documentation/index.html
-            //"/<ApplicationName>;component/Icons/folder.png"
-            // Envoie la source (page web) a afficher
-            //WebRenderer();
-
-
-            System.Uri uri1 = new Uri(@"F:/DUT INFORMATIQUE/2ND/TUT/ChtemeleSurface/ChtemeleSurfaceApplication/Documentation/");
-            System.Uri uri2 = new Uri(@"F:/DUT INFORMATIQUE/2ND/TUT/ChtemeleSurface/ChtemeleSurfaceApplication/Documentation/index.html");
-
-            Uri relativeUri = uri2.MakeRelativeUri(uri1);
-
-            Console.WriteLine(relativeUri.ToString());
-
-            Uri muri = new Uri(@".\\Resources\\Documentation\\index.html", UriKind.Relative);
-            Uri muri2 = new Uri(@"index.html", UriKind.Relative);
-            //WB1.Navigate(muri2);
+            WebRenderer();
         }
 
         
@@ -71,9 +48,6 @@ namespace ChtemeleSurfaceApplication
         private void WebRenderer()
         {
             WB1.Navigate(new Uri(getFileUrl()));
-
-            //Uri uri = new Uri(@"Images\Backgrounds\Background1.png", UriKind.Relative);
-            //string temp = uri.ToString(); //uri.OriginalString
         }
     }
 }
