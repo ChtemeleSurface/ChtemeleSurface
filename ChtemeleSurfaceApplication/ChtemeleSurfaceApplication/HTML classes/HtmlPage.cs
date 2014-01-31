401b777197d7a8ba34bc283b98395bb1283a428c
Thaledric <thaledric@gmail.com>
2014-01-31 09:49:58 +0100

    Essai autoindent

--- ChtemeleSurfaceApplication/ChtemeleSurfaceApplication/HTML classes/HtmlPage.cs
+++ ChtemeleSurfaceApplication/ChtemeleSurfaceApplication/HTML classes/HtmlPage.cs
@@ -52,83 +52,6 @@ namespace ChtemeleSurfaceApplication.HTML_classes
                     _baliseDIV2.addContent(_baliseH3);
 
 
-           /*
-            //instantiation balises test
-            HtmlElement _baliseH1 = new HtmlElement("H1");
-            _baliseH1.tagContent.Add(new HtmlTag("h1", HtmlTag.HTMLTagType.OPENTAG));
-            _baliseH1.tagContent.Add(new HtmlText("Ceci est un titre."));
-            _baliseH1.tagContent.Add(new HtmlTag("h1", HtmlTag.HTMLTagType.ENDTAG));
-
-            HtmlElement _baliseH2 = new HtmlElement("H2");
-            _baliseH2.tagContent.Add(new HtmlTag("h2", HtmlTag.HTMLTagType.OPENTAG));
-            _baliseH2.tagContent.Add(new HtmlText("Ceci est un sous-titre."));
-            _baliseH2.tagContent.Add(new HtmlTag("h2", HtmlTag.HTMLTagType.ENDTAG));
-
-            HtmlElement _baliseP = new HtmlElement("P");
-            _baliseP.tagContent.Add(new HtmlTag("p", HtmlTag.HTMLTagType.OPENTAG));
-            _baliseP.tagContent.Add(new HtmlText("Ceci est un paragraphe."));
-            _baliseP.tagContent.Add(new HtmlTag("p", HtmlTag.HTMLTagType.ENDTAG));
-            _baliseP.attributes.Add(new HtmlTagAttribute("class", "monparagraphe"));
-
-            HtmlElement _baliseDIV = new HtmlElement("DIV");
-            _baliseDIV.tagContent.Add(new HtmlTag("div", HtmlTag.HTMLTagType.OPENTAG));
-            _baliseDIV.tagContent.Add(_baliseP);
-            _baliseDIV.tagContent.Add(new HtmlTag("div", HtmlTag.HTMLTagType.ENDTAG));
-
-            HtmlElement _baliseBLOCKQUOTE = new HtmlElement("BLOCKQUOTE");
-            _baliseBLOCKQUOTE.tagContent.Add(new HtmlTag("blockquote", HtmlTag.HTMLTagType.OPENTAG));
-            _baliseBLOCKQUOTE.tagContent.Add(new HtmlTag("blockquote", HtmlTag.HTMLTagType.ENDTAG));
-
-            HtmlElement _baliseHEADER = new HtmlElement("HEADER");
-            _baliseHEADER.tagContent.Add(new HtmlTag("header", HtmlTag.HTMLTagType.OPENTAG));
-            _baliseHEADER.tagContent.Add(new HtmlTag("header", HtmlTag.HTMLTagType.ENDTAG));
-
-            HtmlElement _baliseFOOTER = new HtmlElement("FOOTER");
-            _baliseFOOTER.tagContent.Add(new HtmlTag("footer", HtmlTag.HTMLTagType.OPENTAG));
-            _baliseFOOTER.tagContent.Add(new HtmlTag("footer", HtmlTag.HTMLTagType.ENDTAG));
-
-            HtmlElement _baliseASIDE = new HtmlElement("ASIDE");
-            _baliseASIDE.tagContent.Add(new HtmlTag("aside", HtmlTag.HTMLTagType.OPENTAG));
-            _baliseASIDE.tagContent.Add(new HtmlTag("aside", HtmlTag.HTMLTagType.ENDTAG));
-
-            HtmlElement _baliseSTRONG = new HtmlElement("STRONG");
-            _baliseSTRONG.tagContent.Add(new HtmlTag("strong", HtmlTag.HTMLTagType.OPENTAG));
-            _baliseSTRONG.tagContent.Add(new HtmlTag("strong", HtmlTag.HTMLTagType.ENDTAG));
-
-            HtmlElement _baliseEM = new HtmlElement("EM");
-            _baliseEM.tagContent.Add(new HtmlTag("em", HtmlTag.HTMLTagType.OPENTAG));
-            _baliseEM.tagContent.Add(new HtmlTag("em", HtmlTag.HTMLTagType.ENDTAG));
-
-            HtmlElement _baliseA = new HtmlElement("A");
-            _baliseA.tagContent.Add(new HtmlTag("a", HtmlTag.HTMLTagType.OPENTAG));
-            _baliseA.tagContent.Add(new HtmlTag("a", HtmlTag.HTMLTagType.ENDTAG));
-
-            HtmlElement _baliseBR = new HtmlElement("BR");
-            _baliseBR.tagContent.Add(new HtmlTag("br", HtmlTag.HTMLTagType.OPENTAG));
-            _baliseBR.tagContent.Add(new HtmlTag("br", HtmlTag.HTMLTagType.ENDTAG));
-
-            HtmlElement _baliseHR = new HtmlElement("HR");
-            _baliseHR.tagContent.Add(new HtmlTag("hr", HtmlTag.HTMLTagType.OPENTAG));
-            _baliseHR.tagContent.Add(new HtmlTag("hr", HtmlTag.HTMLTagType.ENDTAG));
-
-            HtmlElement _baliseIMG = new HtmlElement("IMG");
-            _baliseIMG.tagContent.Add(new HtmlTag("img", HtmlTag.HTMLTagType.OPENTAG));
-            _baliseIMG.tagContent.Add(new HtmlTag("img", HtmlTag.HTMLTagType.ENDTAG));
-
-            //body
-            _mainTag = new HtmlElement("BODY");
-            _mainTag.tagContent.Add(new HtmlTag("body", HtmlTag.HTMLTagType.OPENTAG));
-
-            _mainTag.tagContent.Add(_baliseH1);
-            _mainTag.tagContent.Add(_baliseH2);
-            _mainTag.tagContent.Add(_baliseDIV);
-
-            _mainTag.tagContent.Add(new HtmlTag("body", HtmlTag.HTMLTagType.ENDTAG));
-
-            */
-
-
-
         }
 
         public string renderHTML(){
@@ -144,8 +67,16 @@ namespace ChtemeleSurfaceApplication.HTML_classes
         public string autoIndent(string res)
         {
             indentLevel = 0;
-            int indentCount = 0;
 
+            //On parcout toutes les balises et les retours à la ligne de la séquence.
+
+            string pattern = @"(\n|(<(?</?tagname>\w+)>))";    // capture soit "\n", soit "<{tagname}>", soit "</{tagname}>"
+
+            //Regex.Replace(res, pattern, fetchIndentItem);
+
+            //Match m = Regex. (res, @"^</?(\w+)");
+
+            /*
             string[] lines = res.Split('\n');
 
             //on parcours toutes les lignes du code HTML
@@ -176,8 +107,30 @@ namespace ChtemeleSurfaceApplication.HTML_classes
             }
 
             res = String.Join("\n", lines);
+            */
+
 
             return res;
+        }
+
+        private static string fetchIndentItem(Match item)
+        {
+            string s = item.ToString();
+
+            if (s == "\n")  //nouvelle ligne
+            {
+                s = new String(' ', indentSize * indentLevel) + s;
+            }
+            else if(Regex.IsMatch(s, @"^<\w+>$"))       //balise ouvrante
+            {
+                indentLevel++;
+            }
+            else    //balise fermante
+            {
+                indentLevel--;
+            }
+
+            return s;
         }
     }
 }
