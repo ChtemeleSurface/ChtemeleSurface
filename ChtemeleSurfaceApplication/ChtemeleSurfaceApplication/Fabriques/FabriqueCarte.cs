d718cd91be844f60036e1171634d188c4f450f5a
Janisse <j_clement_42@live.fr>
2014-02-12 00:09:32 +0100

    /!\ Big Update  - Corige le codage de gros porc de mon post precedant (appelle le destroy correctement)  - Corrige le dictionnaire des assoc Tag/Carte  - Appelle une carte losrque lors du premier tag sur la table  - Nettoie quelque fichier (commentaire de test)

--- ChtemeleSurfaceApplication/ChtemeleSurfaceApplication/Fabriques/FabriqueCarte.cs
+++ ChtemeleSurfaceApplication/ChtemeleSurfaceApplication/Fabriques/FabriqueCarte.cs
@@ -12,8 +12,6 @@ namespace ChtemeleSurfaceApplication.Fabriques
 {
     class FabriqueCarte
     {
-       
-
         /*---- Cartes Addons ----*/
         //Antivirus
         public static Antivirus CreateAntivirus()
